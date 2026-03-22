d8674a49fd680c2089eadb34ebedd9679424af2d
Shreya <shreyaa700766@gmail.com>
2026-03-22 18:45:53 +0530

    motivation

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function openFeatures() {
   });
 }
 
-//openFeatures()
+openFeatures()
 
 function todoList() {
   var currentTask = [];
@@ -30,7 +30,7 @@ function todoList() {
   function renderTask() {
     var allTask = document.querySelector(".allTask");
 
-    var sum = "";
+    let sum = "";
 
     currentTask.forEach(function (elem, idx) {
       sum += `<div class="task">
@@ -75,4 +75,54 @@ function todoList() {
 
   
 }
-//todoList()
+todoList()
+
+
+
+function dailyPlanner(){
+  var dayPlanner= document.querySelector('.day-planner')
+var dayPlanData=JSON.parse(localStorage.getItem('dayPlanData'))||{}
+
+
+var hours=Array.from({length:18},(_,idx)=> `${6+idx}:00 - ${7+idx}:00`)
+
+
+
+var wholeDaySum=''
+hours.forEach(function(elem,idx){
+
+  var savedData=dayPlanData[idx] || ''
+
+  wholeDaySum+=`<div class="day-planner-time">
+                    <p>${elem}</p>
+                    <input id=${idx} type="text" placeholder="..." value=${savedData}>
+                </div>`
+})
+
+
+dayPlanner.innerHTML=wholeDaySum
+
+var dayPlannerInput=document.querySelectorAll('.day-planner input')
+
+dayPlannerInput.forEach(function(elem){
+  elem.addEventListener('input',function(){
+    dayPlanData[elem.id]=elem.value
+    localStorage.setItem('dayPlanData',JSON.stringify(dayPlanData))
+
+  })
+})
+}
+
+dailyPlanner()
+
+var motivationQuote=document.querySelector('.motivation-2 h1')
+var motivationAuthor=document.querySelector('.motivation-3 h2')
+
+async function fetchQuote(){
+  let response= await fetch('https://api.quotable.io/random')
+  let data=await response.json()
+  motivationQuote.innerHTML=data.content
+  motivationAuthor.innerHTML=data.author
+
+}
+fetchQuote()
